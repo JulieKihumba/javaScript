8123cb30d0d36f21e7b89e807c08cb7917edb262
JulieKihumba <eng.juliekihumba@gmail.com>
2026-01-08 06:45:35 +0300

    add initial JavaScript functionality and link app.js in HTML

--- counter-app/app.js
+++ counter-app/app.js
@@ -1,2 +1,11 @@
 countEl = document.getElementById("count-el");
 saveEL = document.getElementById("save-el");
+
+count=0
+
+function increment(){
+    console.log("The button was clicked")
+}
+function save(){
+     console.log("The button was clicked")
+}
